e12eb765571fedd80ad181c8e99497cd13e83d61
jiwonyoon <yjw5207@naver.com>
2021-03-19 15:14:29 +0900

    navigation var and footer module

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from bs4 import BeautifulSoup
+from .models import Post
 
 # Create your tests here.
+
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_post_list(self):
+        # 블로그로 들어간다
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
