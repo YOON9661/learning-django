9bfb27d580ad828ad5e6c73bf0a6c6cdf1e87ded
jiwonyoon <yjw5207@naver.com>
2021-03-19 19:34:20 +0900

    add category function

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+import os
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
@@ -9,10 +22,18 @@ class Post(models.Model):
     head_image = models.ImageField(upload_to='blog/image/%Y/%m/%d/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # author은 추후에 작성
 
+    # author은 추후에 작성
+    # models.CASCADE => 포스트의 작성자를 지우면 그 이 포스트도 같이 삭제한다는 뜻
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL)
+
+    # 작성자 정보 출력
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
+
+
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,25 @@
-from .models import Post
+from .models import Post, Category
 from django.views.generic import ListView, DetailView
 
 # Create your views here.
 class PostList(ListView):
     model = Post
     ordering = '-pk'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
     # template_name = 'blog/post_list.html'
 
 class PostDetail(DetailView):
     model = Post
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
 
 # 함수형으로 나타내기
 # def index(request):
