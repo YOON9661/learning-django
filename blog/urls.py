e39559f4717c30cfed8832647fbdb21da658f955
jiwonyoon <yjw5207@naver.com>
2021-03-20 20:03:37 +0900

    댓글 삭제 기능 추가하기

--- blog/urls.py
+++ blog/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     path('create_post/', views.PostCreate.as_view()),
     path('update_post/<int:pk>', views.PostUpdate.as_view()),
     path('<int:pk>/new_comment/', views.new_comment),
+    path('update_comment/<int:pk>/', views.CommentUpdate.as_view()),
+    path('delete_comment/<int:pk>/', views.delete_comment),
     # path('', views.index),
     # path('<int:pk>/', views.single_post_page)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
@@ -148,5 +148,24 @@ def new_comment(request, pk):
                 return redirect(comment.get_absolute_url())
         else:
             return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
+
+class CommentUpdate(LoginRequiredMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(CommentUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            return PermissionDenied
+
+def delete_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post = comment.post
+    if request.user.is_authenticated and request.user == comment.author:
+        comment.delete()
+        return redirect(post.get_absolute_url())
     else:
         raise PermissionDenied
